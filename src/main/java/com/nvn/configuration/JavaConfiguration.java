5da1e6ff99fe82a05200c8c6962f0dce0dca2606
namtao <vanam.pxhn@gmail.com>
2020-01-22 21:15:47 +0700

    22/01/2020

--- src/main/java/com/nvn/configuration/JavaConfiguration.java
+++ src/main/java/com/nvn/configuration/JavaConfiguration.java
@@ -8,6 +8,7 @@ import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -20,12 +21,17 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
 
 import com.nvn.entities.Account;
 import com.nvn.entities.Category;
@@ -40,7 +46,11 @@ public class JavaConfiguration implements WebMvcConfigurer {
 
 	@Autowired
 	private Environment env;
+	
+	@Autowired
+	 private ApplicationContext applicationContext;
 
+	//JSP	
 	@Bean
 	public InternalResourceViewResolver viewResolver() {
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
@@ -49,6 +59,35 @@ public class JavaConfiguration implements WebMvcConfigurer {
 		viewResolver.setSuffix(".jsp");
 		return viewResolver;
 	}
+	
+	//Thymeleaf
+	/*@Bean
+	  public SpringResourceTemplateResolver templateResolver() {
+	    SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+	    templateResolver.setApplicationContext(this.applicationContext);
+	    templateResolver.setPrefix("./resources/HTML/");
+	    templateResolver.setSuffix(".html");
+	    templateResolver.setTemplateMode(TemplateMode.HTML);
+	    templateResolver.setCacheable(true);
+	    templateResolver.setCharacterEncoding("UTF-8");
+	    return templateResolver;
+	  }
+	  @Bean
+	  public SpringTemplateEngine templateEngine() {
+	    SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+	    templateEngine.setTemplateResolver(templateResolver());
+	    templateEngine.setEnableSpringELCompiler(true);
+	    return templateEngine;
+	  }
+	  
+	   @Bean
+	    public ViewResolver thymeleafViewResolver(){
+	        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+	        viewResolver.setTemplateEngine(templateEngine());
+	        viewResolver.setCharacterEncoding("UTF-8");
+	        return viewResolver;
+	    }
+*/
 
 	@Bean
 	public DataSource dataSource() {
